680f286fd20e45e085024ef1ad60bb140a9f63c0
Michael Howitz <icemac@gmx.net>
2025-09-12 09:51:12 +0200

    Back to development: 7.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 
 setup(name='zope.untrustedpython',
-      version='7.0',
+      version='7.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Untrusted Python Library',
