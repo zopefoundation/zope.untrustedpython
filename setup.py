1f2098cc00f7af8eea8a55bf580fa1f9e38e35f6
Michael Howitz <icemac@gmx.net>
2026-03-20 09:07:10 +0100

    Update Python version support. (#17)
    * Drop support for Python 3.9.
    
    * Add support for Python 3.14.
    
    * Configuring for pure-python
    
    * Update Python version support.
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -16,57 +16,10 @@
 # When developing and releasing this package, please follow the documented
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
-"""Setup for zope.security package
+"""Setup for zope.untrustedpython package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-setup(name='zope.untrustedpython',
-      version='7.1.dev0',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      description='Zope Untrusted Python Library',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')
-      ),
-      keywords="zope untrusted python",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope :: 3',
-      ],
-      url='http://github.com/zopefoundation/zope.untrustedpython',
-      license='ZPL-2.1',
-      python_requires='>=3.9',
-      install_requires=[
-          'RestrictedPython >= 4',
-          'setuptools',
-          'zope.security',
-      ],
-      extras_require={
-          'docs': ['Sphinx'],
-      },
-      include_package_data=True,
-      zip_safe=True,
-      )
+# See pyproject.toml for package metadata
+setup()
